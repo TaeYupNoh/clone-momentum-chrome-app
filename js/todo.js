e1fad863173b3577079be970a5fe1cd42e15f5cc
yeop <shxoduq2@naver.com>
2023-01-31 23:35:34 +0900

    feat: Add todo.js & add img

---
+++ js/todo.js
@@ -0,0 +1,38 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("input");
+// === const toDoInput = document.querySelector("#todo-form input")
+const toDoList = document.getElementById("todo-list");
+
+const toDos = [];
+
+function deleteTodo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
+function saveToDos() {
+  localStorage.setItem("todos", JSON.stringify(toDos));
+}
+
+function paintToDo(newToDo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerText = newToDo;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteTodo);
+  li.appendChild(span);
+  li.appendChild(button);
+  toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  const newToDo = toDoInput.value;
+  toDoInput.value = "";
+  toDos.push(newToDo);
+  paintToDo(newToDo);
+  saveToDos();
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
